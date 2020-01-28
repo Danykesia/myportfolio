e647ce04df74c2866d4c2d14a684ac0d4f6af463
danykesia <danielekesia@outlook.com>
2020-01-28 11:46:21 -0300

    apply a avatar to the heading text

---
+++ src/components/Me.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <img src="../assets/avatar.png" alt="Minha foto">
+  </div>
+</template>
+
+<style lang="scss" scoped>
+img {
+  display: flex;
+  // margin: auto;
+  padding-top: 20px;
+  margin-top: -10px;
+  width: 140px;
+  height: 180px;
+}
+</style>
