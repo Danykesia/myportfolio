cee8867267cf9ed309c4b1a8e18a71d089841b27
danykesia <danielekesia@outlook.com>
2020-01-28 10:58:36 -0300

    apply a white border

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,14 @@
 <template>
   <header>
     <HeadingText/>
+    <Button/>
   </header>
 </template>
 
 <style lang="scss" scoped>
 header {
   background: linear-gradient(to left top,#9f56f7, #ad89fa, #b7a0f9);
+  border: 3px solid #ffffff;
   height: 100vh;
   width: 100%;
 }
@@ -14,11 +16,13 @@ header {
 
 <script>
 import HeadingText from '@/components/HeadingText.vue';
+import Button from '@/components/Button.vue';
 
 export default {
   name: 'home',
   components: {
     HeadingText,
+    Button,
   },
 };
 </script>
