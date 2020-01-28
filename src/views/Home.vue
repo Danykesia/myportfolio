ee3623fe58b0fa5a4f28b0577986325b9183d375
danykesia <danielekesia@outlook.com>
2020-01-28 14:10:48 -0300

    add new component 'Portfolio'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <Header/>
+    <Portfolio/>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header.vue';
+import Portfolio from '@/components/Portfolio.vue';
 
 export default {
   name: 'home',
   components: {
     Header,
+    Portfolio,
   },
 };
 </script>
