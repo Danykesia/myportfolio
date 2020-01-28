bcea32741449937d9393f017d08f70c50f954a3a
danykesia <danielekesia@outlook.com>
2020-01-28 14:07:55 -0300

    add 'h2' removed from 'HeadingText'

--- src/components/Me.vue
+++ src/components/Me.vue
@@ -1,16 +1,29 @@
 <template>
-  <div>
+  <div class="perfil">
     <img src="../assets/avatar.png" alt="Minha foto">
+    <h2>Olá, eu sou Daniele Késia!</h2>
   </div>
 </template>
 
 <style lang="scss" scoped>
+h2 {
+  font-weight: 500;
+  margin-top: -8rem;
+  margin-left: 9.37rem;
+  font-size: 1.1rem;
+  color: #f3ecec;
+  font-weight: bold;
+}
+
 img {
+  width: 8rem;
+  height: 9rem;
+}
+
+.perfil {
   display: flex;
-  // margin: auto;
-  padding-top: 20px;
-  margin-top: -10px;
-  width: 140px;
-  height: 180px;
+  justify-content: space-evenly;
+  flex-direction: column;
+  padding-top: 1rem;
 }
 </style>
