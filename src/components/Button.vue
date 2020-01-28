f52b3a2c90e0cefd1a4d77e5fd3d5a30f28c2a38
danykesia <danielekesia@outlook.com>
2020-01-28 11:47:26 -0300

    set button styles

---
+++ src/components/Button.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <button><a href="#portfolio">Veja meus trabalhos</a></button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+button {
+  border: 2px solid pink;
+  height: 40px;
+  width: 160px;
+  text-align: center;
+  background: transparent;
+  margin-top: 20px;
+  box-shadow: 4px 5px 13px 0px #080839;
+}
+
+a {
+  color: #fff;
+  font-size: .9rem;
+  font-weight: bold;
+}
+</style>
