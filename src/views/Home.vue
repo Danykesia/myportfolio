0f8077b1b505facb4fb0d1697a212ec3edf1f45a
danykesia <danielekesia@outlook.com>
2020-01-29 16:23:43 -0300

    add component contact

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,21 @@
   <div class="home">
     <Header/>
     <Portfolio/>
+    <Contact/>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header.vue';
 import Portfolio from '@/components/Portfolio.vue';
+import Contact from '@/components/Contact.vue';
 
 export default {
   name: 'home',
   components: {
     Header,
     Portfolio,
+    Contact,
   },
 };
 </script>
