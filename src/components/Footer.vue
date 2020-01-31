66276e1213c6069fcde04e31b07e088a72579f27
danykesia <danielekesia@outlook.com>
2020-01-31 07:23:13 -0300

    add footer content

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,3 +1,16 @@
 <template>
-  <footer></footer>
+  <footer class="copyright">
+    <p>&copy; Copyright 2020 - Daniele Késia - Todos os direitos reservados</p>
+  </footer>
 </template>
+
+<style lang="scss" scoped>
+.copyright {
+  background: rgb(19, 0, 37);
+  color: white;
+  padding: 1rem;
+  // display: flex;
+  text-align: center;
+  font-size: .8rem;
+}
+</style>
