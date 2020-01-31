76614cd2dc4f7efb1baa4cd3bb2ec6a27946da80
danykesia <danielekesia@outlook.com>
2020-01-31 07:22:25 -0300

    create componente and settings

---
+++ src/components/Form.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="form-wrapper">
+    <form action="">
+      <div class="name">
+        <div class="col-1">
+          <label for="name">Nome<span>*</span></label>
+        </div>
+        <div class="col-2">
+          <input type="text" id="name" name="name">
+        </div>
+      </div>
+
+      <div class="email">
+        <div class="col-1">
+          <label for="email">E-mail<span>*</span></label>
+        </div>
+        <div class="col-2">
+          <input type="text" id="email" name="email">
+        </div>
+      </div>
+
+      <div class="subject">
+        <div class="col-1">
+          <label for="subject">Mensagem<span>*</span></label>
+        </div>
+        <div class="col-2">
+          <textarea name="subject" id="subject"
+            style="heigth: 400px;">
+          </textarea>
+        </div>
+      </div>
+
+      <div class="button">
+        <button type="submit">ENVIAR</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.form-wrapper {
+  display: flex;
+  flex-direction: column;
+  // background: black;
+  margin-top: 1rem;
+}
+
+.name {
+  display: flex;
+  flex-direction: column;
+}
+
+.email {
+  display: flex;
+  flex-direction: column;
+  margin-top: 1rem;
+}
+
+.col-2 {
+  width: 80%;
+}
+
+.subject {
+  display: flex;
+  flex-direction: column;
+  margin-top: 1rem;
+}
+
+span {
+  color: red;
+}
+
+input[type=text], select, textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  resize: vertical;
+  background:rgb(211, 202, 202);
+  border: 3px solid transparent;
+  outline: none;
+
+  &:focus {
+    background: #e0dbdb;
+    border: 3px solid #fd8cd7;
+  }
+}
+
+label {
+  padding: 10px 0;
+  display: inline-block;
+  color: rgb(190, 187, 187);
+}
+
+textarea{
+  height: 100px;
+}
+
+button {
+  color: #fff;
+  background: rgb(217, 139, 253);
+  border: 3px solid transparent;
+  border-radius: 0 3px 3px 0;
+  padding: 12px 20px;
+  border: nome;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+  z-index: 200;
+
+  &:hover {
+    background: transparent;
+    border: 3px solid white;
+  }
+}
+
+.button {
+  display: flex;
+  justify-content: flex-start;
+  margin-top: 1rem;
+  margin-bottom: 2rem;
+}
+
+        @media (min-width: 768px) {
+          .container {
+            padding-right: 0;
+          }
+        }
+</style>
